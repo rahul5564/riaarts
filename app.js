4026cbf5c84d48493aafd7f6af3748a15da8dc82
rahul <sraghavendra5564@gmail.com>
2021-09-14 19:51:05 +0530

    updating

---
+++ app.js
@@ -0,0 +1,16 @@
+var imgs = document.getElementById('imgs');
+
+var img = document.querySelectorAll('#imgs img');
+
+var idx = 0;
+
+function run(){
+    idx++;
+    if(idx > img.length - 2){
+        idx = 0;
+    }
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+    console.log(idx)
+}
+
+setInterval(run,1500)
